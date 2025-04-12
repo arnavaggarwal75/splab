1ae06bd00f882bd5c6368d81dae7236db8c282db
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-04-12 03:49:45 -0400

    initial firestore setup

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from google.cloud import firestore
 
 app = FastAPI()
+
+db = firestore.Client.from_service_account_json('./serviceAccountKey.json')
 
 @app.get("/")
 def read_root():
