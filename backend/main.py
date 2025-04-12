4ca62c9cecad4ef1af96deafba988d7ab9a4c1f5
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-04-12 04:19:26 -0400

    test read and write endpoints

--- backend/main.py
+++ backend/main.py
@@ -8,3 +8,18 @@ db = firestore.Client.from_service_account_json('./serviceAccountKey.json')
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+# Sample Read and Write endpoints for Firestore
+@app.post("/test-write")
+def test_write():
+    doc_ref = db.collection("test").document("ping")
+    doc_ref.set({"message": "pong", "active": True})
+    return {"status": "written"}
+
+@app.get("/test-read")
+def test_read():
+    doc = db.collection("test").document("ping").get()
+    if doc.exists:
+        return doc.to_dict()
+    return {"error": "Document not found"}
